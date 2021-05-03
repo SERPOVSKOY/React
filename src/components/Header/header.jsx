0ba374df7a84c4acf3a1cd8bd3a67e0c0792e191
Alexey Serpovskoy <SERPOVSKOY.96@mail.ru>
2021-05-03 12:16:13 +0300

    added test components

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <ul>
+        <li><a href="#1">Главная</a></li>
+        <li><a href="#2">Сообщения</a></li>
+        <li><a href="#3">Новости</a></li>
+        <li><a href="#4">Друзья</a></li>
+      </ul>
+    )
+}
+
+export default Header;
